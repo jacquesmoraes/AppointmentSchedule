206e0b18ed4a594c700b740c3e1f9fa5a82997e0
jacquesmoraes <jacquesbmoraes@hotmail.com>
2022-11-02 15:03:35 -0300

    Layout Changes

---
+++ Appointment/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Appointment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Appointment.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
